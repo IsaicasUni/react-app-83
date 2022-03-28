4bd43f33216c0888cf86724a968ac9b6f2bd9612
Alberto Casillas <isaicasillas2401@gmail.com>
2022-03-28 12:12:08 -0700

    tabla-practica

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Llama el contenido de la constante
+import Datos from './Componente';
+
+// Imprime los valores de la tabla
+ReactDOM.render(
+  <Datos />,
+  document.getElementById('root')
+);
+
